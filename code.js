35b1723c2c4de29835f4771afffca941036e89c3
Knights879 <42558531+Knights879@users.noreply.github.com>
2019-09-19 20:14:18 -0400

    progress

--- code.js
+++ code.js
@@ -7,9 +7,6 @@ var userId = 0;
 var firstName = "";
 var lastName = "";
 
-var contactName = "";
-var contactEmail = "";
-var contactPhoneNumber = "";
 var contactsList = [];
 
 function doLogin()
@@ -30,10 +27,40 @@ function doLogin()
 	xhr.open("POST", url, false);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-   try {
-      //
+   try
+   {
+      xhr.onreadystatechange = function()
+      {
+         if (this.readyState == 4 && this.status == 200)
+         {
+            var jsonObject = JSON.parse(xhr.responseText);
+
+      		userId = jsonObject.id;
+
+      		if (userId < 1)
+      		{
+      			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+      			return;
+      		}
+
+      		firstName = jsonObject.firstName;
+      		lastName = jsonObject.lastName;
+
+      		document.getElementById("userName").innerHTML = firstName + " " + lastName;
+
+      		document.getElementById("loginName").value = "";
+      		document.getElementById("loginPassword").value = "";
+
+      		hideOrShow("loggedInDiv", true);
+      		hideOrShow("accessUIDiv", true);
+      		hideOrShow("loginDiv", false);
+
+      		xhr.send(jsonPayload);  // Move out of function to bottom of try block
+         }
+      }
    }
-   catch(err) {
+   catch(err)
+   {
       document.getElementById("loginResult").innerHTML = err.message;
    }
 }
@@ -44,10 +71,11 @@ function doLogout()
    firstName = "";
    lastName = "";
 
-   //
-
    document.getElementById("loginName").value = "";
    document.getElementById("loginPassword").value = "";
+   document.getElementById("contactName").value = "";
+   document.getElementById("contactEmail").value = "";
+   document.getElementById("contactPhoneNumber").value = "";
 
    hideOrShow("loggedInDiv", false);
 	hideOrShow("accessUIDiv", false);
@@ -69,4 +97,60 @@ function hideOrShow(elementId, showState)
 	document.getElementById(elementId).style.display = dis;
 }
 
-//
+function doRegistration()
+{
+   hideOrShow("loginDiv", false);
+   hideOrShow("registerDiv", true);
+}
+
+function sumbitRegistration()
+{
+   //
+}
+
+function addContact()
+{
+   var cFName = document.getElementById("cFirstName").value;
+   var cLName = document.getElementById("cLastName").value;
+   var cPhoneNum = document.getElementById("cPhoneNumber").value;
+   var cEmail = document.getElementById("cEmail").value;
+   document.getElementById("contactAddResult").innerHTML = "";
+
+   var jsonPayload = '{"cFirstName" : "' + cFName + '", "cLastName" : "' + cLName + '", "cPhoneNum" : "' + cPhoneNum + '", "cEmail" : "' + cEmail + '", "userId" : "' + userId + '"}';
+   var url = urlBase + '/AddContact.' + extension;
+
+   var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+   try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+}
+
+function retrieveContacts()
+{
+   //
+}
+
+function searchContact()
+{
+   //
+}
+
+function deleteContact()
+{
+   //
+}
